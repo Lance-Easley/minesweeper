d26da446845b998b116eeca4f022232ba312f3f2
Lance Easley <leasley21@basecampcodingacademy.org>
2021-01-12 14:20:46 -0600

    Technically working game

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ RED = (255, 0, 0)
 SEA_BLUE = (0, 175, 255)
 GREY = (80, 80, 80)
 ORANGE = (255,180,0)
+YELLOW = (255,255,0)
 
 # This sets the WIDTH and HEIGHT of each grid location
 WIDTH = 30
@@ -19,7 +20,7 @@ MARGIN = 3
 pygame.init()
 
 pygame.font.init()
-font = pygame.font.Font("freesansbold.ttf", 32)
+font = pygame.font.Font("freesansbold.ttf", 26)
 
 screen_x = 828
 screen_y = 828
@@ -31,16 +32,20 @@ pygame.display.set_caption('Minesweeper')
 size = 25
 
 play_grid = []
+game_grid = []
 for row in range(size):
     # Add an empty array that will hold each cell
     # in this row
     play_grid.append([])
+    game_grid.append([])
     for column in range(size):
         rand = random.randint(0, (size//4) - 1)
         if rand == 0 and row != 0 and row != size - 1 and column != 0 and column != size - 1:
             play_grid[row].append(9)  # Append a cell
         else:
             play_grid[row].append(0)
+        game_grid[row].append(0)
+
 for row in range(size):
     for column in range(size):
         if play_grid[row][column] != 9:
@@ -77,36 +82,83 @@ while not done:
         if event.type == pygame.MOUSEBUTTONDOWN:
             # User clicks the mouse. Get the position
             pos = pygame.mouse.get_pos()
-            if pos[0] <= 450 and pos[1] <= 445:
-                    # Change the x/y screen coordinates to grid coordinates
-                    column = pos[0] // (WIDTH + MARGIN)
-                    row = pos[1] // (HEIGHT + MARGIN)
-                    print("Grid coordinates: ", column, row)
-                    val_text = font.render(f'{play_grid[row][column]}', True, WHITE)
-                    val_textRect = val_text.get_rect()
-                    val_textRect.center = (578, 90)
-                    if play_grid[row][column] == 9:
-                        pass
+            # Change the x/y screen coordinates to grid coordinates
+            column = pos[0] // (WIDTH + MARGIN)
+            row = pos[1] // (HEIGHT + MARGIN)
+            print("Grid coordinates: ", column, row)
+            if play_grid[row][column] == 9:
+                pygame.quit()
+                exit()
+            if game_grid[row][column] == 0:
+                game_grid[row][column] = 1
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_f:
+                # User clicks the mouse. Get the position
+                pos = pygame.mouse.get_pos()
+                # Change the x/y screen coordinates to grid coordinates
+                column = pos[0] // (WIDTH + MARGIN)
+                row = pos[1] // (HEIGHT + MARGIN)
+                print("Grid coordinates: ", column, row)
+                if game_grid[row][column] == 0:
+                    game_grid[row][column] = 9
+                elif game_grid[row][column] == 9:
+                    game_grid[row][column] = 0
 
 
     for row in range(size):
         for column in range(size):
             color = GREY
-            if play_grid[row][column] == 9:
-                color = RED
-            elif play_grid[row][column] != 0 and play_grid[row][column] != 9:
+            if game_grid[row][column] == 1:
+                if play_grid[row][column] == 9:
+                    color = RED
+                elif play_grid[row][column] != 0 and play_grid[row][column] != 9:
+                    color = ORANGE
+                pygame.draw.rect(
+                    screen,
+                    color,
+                    [
+                        (MARGIN + WIDTH) * column + MARGIN,
+                        (MARGIN + HEIGHT) * row + MARGIN,
+                        WIDTH,
+                        HEIGHT,
+                    ],
+                    0,
+                )
+                # Player text
+                num_text = font.render(f"{play_grid[row][column]}", True, WHITE)
+                num_textRect = num_text.get_rect()
+                num_textRect.center = (
+                    ((MARGIN + WIDTH) * column + MARGIN) + WIDTH // 2, 
+                    ((MARGIN + HEIGHT) * row + MARGIN) + (HEIGHT + MARGIN) // 2,
+                )
+                screen.blit(num_text, num_textRect)
+            elif game_grid[row][column] == 9:
+                color = YELLOW
+                pygame.draw.rect(
+                    screen,
+                    color,
+                    [
+                        (MARGIN + WIDTH) * column + MARGIN,
+                        (MARGIN + HEIGHT) * row + MARGIN,
+                        WIDTH,
+                        HEIGHT,
+                    ],
+                    0,
+                )
+            else:
                 color = ORANGE
-            pygame.draw.rect(
-                screen,
-                color,
-                [
-                    (MARGIN + WIDTH) * column + MARGIN,
-                    (MARGIN + HEIGHT) * row + MARGIN,
-                    WIDTH,
-                    HEIGHT,
-                ],
-                0,
-            )
+                pygame.draw.rect(
+                    screen,
+                    color,
+                    [
+                        (MARGIN + WIDTH) * column + MARGIN,
+                        (MARGIN + HEIGHT) * row + MARGIN,
+                        WIDTH,
+                        HEIGHT,
+                    ],
+                    0,
+                )
     
     clock.tick(60)
 
